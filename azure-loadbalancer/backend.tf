6470639e55f439642587266d58efe5406f11084c
natthidakl <140678535+natthidakl@users.noreply.github.com>
2023-08-22 13:37:06 +0700

    Update backend.tf

--- azure-loadbalancer/backend.tf
+++ azure-loadbalancer/backend.tf
@@ -1,6 +1,8 @@
-terraform {
-  backend "azurerm" {
-    key                     = "##StateFile##"
-    access_key              = "##TerraformStorageCredential##"
+terraform {  
+  backend  "azurerm" {  
+    resource_group_name  = "rg-ais-payment-gateway"
+    storage_account_name = "sbpocstoacc"
+    container_name       = "tfstatetest1"
+    key                  = "terraform.tfstate"
   }
 }

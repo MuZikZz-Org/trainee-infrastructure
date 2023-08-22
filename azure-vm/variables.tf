759ec9413238ba6dd90eeaa449d4c68629e13c71
thunsudamn <140685871+thunsudamn@users.noreply.github.com>
2023-08-22 11:34:08 +0700

    Create variables.tf

---
+++ azure-vm/variables.tf
@@ -0,0 +1,44 @@
+######################################################################
+#                               COMMON
+######################################################################
+
+variable "ARM_SUBSCRIPTION_ID" {
+    description = "Azure Subscription ID (Exiting in common-configs)"
+    type        = string
+}
+
+variable "ARM_TENANT_ID" {
+    description = "Azure Tenant ID (Exiting in common-configs)"
+    type        = string
+}
+variable "ARM_CLIENT_ID" {
+    description = "Azure Client ID (Exiting in common-configs)"
+    type        = string
+}
+variable "ARM_CLIENT_SECRET" {
+    description = "Azure Client Secret (Exiting in common-configs)"
+    type        = string
+}
+
+
+######################################################################
+#                               CUSTOM
+######################################################################
+
+
+
+variable "SRC_IMG_REF_PUBLISHER" {}
+variable "SRC_IMG_REF_OFFER" {}
+variable "SRC_IMG_REF_SKU" {}
+variable "SRC_IMG_REF_VERSION" {}
+
+
+ variable "env_tags" {
+    description = "Tags (Specific to the resource)"
+    type        = map(string)
+}
+
+#variable "resource_group_location" {
+#  description = "Location of resource group."
+#  type        = string
+#}

f384923eeb91007ba20117de261651868b3f9ca5
thunsudamn <140685871+thunsudamn@users.noreply.github.com>
2023-08-28 14:37:10 +0700

    Update backend.tf

--- azure-vm-window/backend.tf
+++ azure-vm-window/backend.tf
@@ -1,6 +1,8 @@
 terraform {
   backend "azurerm" {
-    key                     = "##StateFile##"
-    access_key              = "##TerraformStorageCredential##"
+    resource_group_name  = "rg-ais-payment-gateway"
+    storage_account_name = "sbpocstoacc"
+    container_name       = "tfstatetest2"
+    key                  = "terraform.tfstate"
   }
 }

be3edb8d11b1b96c070ace4e5ba12f31f198a970
thunsudamn <140685871+thunsudamn@users.noreply.github.com>
2023-08-28 14:44:47 +0700

    Update provider.tf

--- azure-vm-window/provider.tf
+++ azure-vm-window/provider.tf
@@ -4,8 +4,8 @@ provider "azurerm" {
       prevent_deletion_if_contains_resources = true
     }
   }
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
-  client_id       = var.workload_access_key
-  client_secret   = var.workload_access_secretkey
+  subscription_id = var.ARM_SUBSCRIPTION_ID
+  tenant_id       = var.ARM_TENANT_ID
+  client_id       = var.ARM_CLIENT_ID
+  client_secret   = var.ARM_CLIENT_SECRET
 }

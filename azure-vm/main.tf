f471067626f473c9893e07dfe89f12294382d059
natthidakl <140678535+natthidakl@users.noreply.github.com>
2023-09-04 12:54:13 +0700

    Update main.tf

--- azure-vm/main.tf
+++ azure-vm/main.tf
@@ -45,7 +45,7 @@ resource "azurerm_network_security_group" "my_terraform_nsg" {
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "9000"
+    destination_port_range     = "22"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }

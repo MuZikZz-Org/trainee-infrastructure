33f47daff0772d4e1c0c6aeeeb5ac64f39b3c5b6
thunsudamn <140685871+thunsudamn@users.noreply.github.com>
2023-09-04 11:03:01 +0700

    Update provisioner.sh

--- azure-vm/provisioner.sh
+++ azure-vm/provisioner.sh
@@ -1,16 +1,29 @@
 #!/usr/bin/env bash
 
+# Install dependencies (e.g., Java)
 sudo apt-get update
 echo "update done"
-# sudo apt-get -y upgrade
-# echo "upgrade done"
+
 sudo apt-get install -y openjdk-11-jdk
 echo "install openjdk done"
+
+# Download and install SonarQube
 wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.0.1.46107.zip
 echo "get sonarqube done"
-sudo apt-get install -y unzip
-echo "install unzip done"
+
 unzip sonarqube-9.0.1.46107.zip
 echo "unzip done"
-sudo mv sonarqube-9.0.1.46107 /opt/sonarqube
+
+mv sonarqube-9.0.1.46107 /opt/sonarqube
 echo "mv done"
+
+# Configure SonarQube settings (e.g., database connection)
+# ...
+
+# Start SonarQube
+/opt/sonarqube/bin/linux-x86-64/sonar.sh start
+
+# Run SonarScanner or other code scanning commands
+# Make sure to configure SonarScanner to connect to your SonarQube instance
+# Example: sonar-scanner -Dsonar.projectKey=my-project-key -Dsonar.sources=.
+

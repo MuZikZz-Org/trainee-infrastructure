4b49eec2759636c3b904e638eb84fd932443a2ab
natthidakl <140678535+natthidakl@users.noreply.github.com>
2023-09-05 14:36:59 +0700

    Update variables.tf

--- azure-vm/variables.tf
+++ azure-vm/variables.tf
@@ -32,10 +32,10 @@ variable "SRC_IMG_REF_OFFER" {}
 variable "SRC_IMG_REF_SKU" {}
 variable "SRC_IMG_REF_VERSION" {}
 
-variable "PUBLIC_IP" {
-  description = "Public IP address for SonarQube"
-  type        = string
-}
+#variable "PUBLIC_IP" {
+ # description = "Public IP address for SonarQube"
+  #type        = string
+#}
 variable "SONARQUBE_PORT" {
   description = "Port for SonarQube"
   type        = number

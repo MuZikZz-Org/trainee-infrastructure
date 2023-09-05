0b725c22925db319991a6daca72fe43113cf6e4c
natthidakl <140678535+natthidakl@users.noreply.github.com>
2023-09-05 09:57:55 +0700

    Update main.tf

--- azure-vm/main.tf
+++ azure-vm/main.tf
@@ -49,6 +49,17 @@ resource "azurerm_network_security_group" "my_terraform_nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
     tags = merge(
      var.env_tags
    )
